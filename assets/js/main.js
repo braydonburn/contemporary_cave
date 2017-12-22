454553a2f0c7ff230e002443adee4985a70f0cec
Braydon <braydon.burn@gmail.com>
2017-12-22 20:14:15 +1000

    Fixed animation for loading screen.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,31 @@
 //On page open, show spinner until page is loaded.
 var $document = $(document),
-  element = $('.spinner'),
-  fadeOut = 'bounce';
+  element = $('.onLoad'),
+  fadeOut = 'fadeOut';
+
+function animationsTest(callback) {
+  // Test if ANY/ALL page animations are currently active
+  var testAnimationInterval = setInterval(function() {
+  }, 25);
+};
 
 var overlay = document.getElementById('overlay');
 window.addEventListener('load', function() {
   element.addClass(fadeOut);
-  overlay.style.display = 'none';
+  animationsTest(function() {
+    overlay.style.display = 'none';
+  })
 })
+
+
 
 // If user scrolls, change transparent nav to coloured
 var $document = $(document),
   $element = $('.navbar'),
   navbarDefault = 'indigo';
-  navbarTransparent = 'smooth-scrolling';
+navbarTransparent = 'smooth-scrolling';
 
-  fadeInDown = 'fadeInDown';
+fadeInDown = 'fadeInDown';
 
 $document.scroll(function() {
   if ($document.scrollTop() >= 100) {
