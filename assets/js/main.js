bfd6ca96f7b640f4b8ea75bf2a21a0e95a9d6333
Braydon <braydon.burn@gmail.com>
2017-12-22 19:48:02 +1000

    Added spinner while loading with fade out animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+//On page open, show spinner until page is loaded.
+var $document = $(document),
+  element = $('.spinner'),
+  fadeOut = 'bounce';
+
+var overlay = document.getElementById('overlay');
+window.addEventListener('load', function() {
+  element.addClass(fadeOut);
+  overlay.style.display = 'none';
+})
+
 // If user scrolls, change transparent nav to coloured
 var $document = $(document),
   $element = $('.navbar'),
