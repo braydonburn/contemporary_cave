87b25b39d01b2e26dfdd90c9dba46d16a7c5dc03
Braydon <braydon.burn@gmail.com>
2017-12-23 20:24:12 +1000

    Fixed not being able to click anything due to fadeOut element not being removed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,11 @@ var $document = $(document),
   element = $('.onLoad'),
   fadeOut = 'fadeOut';
 
-function animationsTest(callback) {
-  // Test if ANY/ALL page animations are currently active
-  var testAnimationInterval = setInterval(function() {
-  }, 25);
-};
-
 var overlay = document.getElementById('overlay');
 window.addEventListener('load', function() {
   element.addClass(fadeOut);
-  animationsTest(function() {
-    overlay.style.display = 'none';
-  })
+  window.setTimeout("overlay.style.display = 'none';", 300);
 })
-
-
 
 // If user scrolls, change transparent nav to coloured
 var $document = $(document),
