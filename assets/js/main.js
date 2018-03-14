80b810b734c1c7099abfb0ed4d3b99bb3699dd5a
Braydon <info@braydonburn.com>
2018-03-14 19:28:03 +1000

    Switched back to full js version.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,101 @@
-var $document=$(document),element=$('.onLoad'),fadeOut='fadeOut';$element=$('.navbar'),navbarDefault='indigo',navbarTransparent='smooth-scrolling',shadow='z-depth-2',fadeInDown='fadeInDown';var overlay=document.getElementById('overlay');window.addEventListener('load',function(){element.addClass(fadeOut);window.setTimeout("overlay.style.display = 'none';",300);})
-$document.scroll(function(){var height=window.screen.height-100;if($document.scrollTop()>=height){$element.addClass(navbarDefault);$element.removeClass(navbarTransparent);$element.addClass(fadeInDown);}else{$element.addClass(navbarTransparent);$element.addClass(shadow);$element.removeClass(navbarDefault);$element.removeClass(fadeInDown);}});$().ready(function(){$('[rel="tooltip"]').tooltip();});function rotateCard(btn){var $card=$(btn).closest('.card-container');console.log($card);if($card.hasClass('hover')){$card.removeClass('hover');}else{$card.addClass('hover');}}var animateHTML=function(){var elems,windowHeight
-var init=function(){elems=document.getElementsByClassName('hidden')
-windowHeight=window.innerHeight
-_addEventHandlers()}
-var _addEventHandlers=function(){window.addEventListener('scroll',_checkPosition)
-window.addEventListener('resize',init)}
-var _checkPosition=function(){for(var i=0;i<elems.length;i++){var posFromTop=elems[i].getBoundingClientRect().top
-if(posFromTop-windowHeight<=0){elems[i].className=elems[i].className.replace('hidden','fadeIn')}}}
-return{init:init}}
+//On page open, show spinner until page is loaded, then execute fadeOut transition
+var $document = $(document),
+  element = $('.onLoad'),
+  fadeOut = 'fadeOut';
+$element = $('.navbar'),
+  navbarDefault = 'indigo',
+  navbarTransparent = 'smooth-scrolling',
+  shadow = 'z-depth-2',
+  fadeInDown = 'fadeInDown';
+
+var overlay = document.getElementById('overlay');
+window.addEventListener('load', function() {
+  element.addClass(fadeOut);
+  window.setTimeout("overlay.style.display = 'none';", 300);
+})
+
+// If user scrolls, change transparent nav to coloured
+$document.scroll(function() {
+  //Every time user scrolls retrieve screen height to avoid resizing errors.
+  var height = window.screen.height - 100;
+  if ($document.scrollTop() >= height) {
+    //user scrolled more than 100 pixels
+    $element.addClass(navbarDefault);
+    $element.removeClass(navbarTransparent);
+
+    $element.addClass(fadeInDown);
+  } else {
+    $element.addClass(navbarTransparent);
+    $element.addClass(shadow);
+    $element.removeClass(navbarDefault);
+
+    $element.removeClass(fadeInDown);
+  }
+});
+
+//When card is hovered over execute rotate.
+$().ready(function() {
+  $('[rel="tooltip"]').tooltip();
+
+});
+
+function rotateCard(btn) {
+  var $card = $(btn).closest('.card-container');
+  console.log($card);
+  if ($card.hasClass('hover')) {
+    $card.removeClass('hover');
+  } else {
+    $card.addClass('hover');
+  }
+}
+
+//Execute fadeIn animation when the element is scrolled into view.
+var animateHTML = function() {
+  var elems,
+    windowHeight
+  var init = function() {
+    elems = document.getElementsByClassName('hidden')
+    windowHeight = window.innerHeight
+    _addEventHandlers()
+  }
+  var _addEventHandlers = function() {
+    window.addEventListener('scroll', _checkPosition)
+    window.addEventListener('resize', init)
+  }
+  var _checkPosition = function() {
+    for (var i = 0; i < elems.length; i++) {
+      var posFromTop = elems[i].getBoundingClientRect().top
+      if (posFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace('hidden', 'fadeIn')
+      }
+    }
+  }
+  return {
+    init: init
+  }
+}
 animateHTML().init()
-$(document).ready(function(){$("#homeButton").click(function(){$('html, body').animate({scrollTop:$("#home").offset().top},1000);});$("#examplesButton").click(function(){$('html, body').animate({scrollTop:$("#examples").offset().top},1000);});$("#contactButton").click(function(){$('html, body').animate({scrollTop:$("#footer").offset().top},1000);});$("#contactButton2").click(function(){$('html, body').animate({scrollTop:$("#footer").offset().top},1000);});});
+
+//Smooth scroll to div
+$(document).ready(function() {
+  $("#homeButton").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#home").offset().top
+    }, 1000);
+  });
+  $("#examplesButton").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#examples").offset().top
+    }, 1000);
+  });
+  $("#contactButton").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#footer").offset().top
+    }, 1000);
+  });
+  $("#contactButton2").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#footer").offset().top
+    }, 1000);
+  });
+});
